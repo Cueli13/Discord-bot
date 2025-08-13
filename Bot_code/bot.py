0e309ff90f781d994f81e6d3045fe681a200590f
Cueli13 <alecueli13@gmail.com>
2025-08-13 23:12:04 +0000

    Update bot.py

--- Bot_code/bot.py
+++ Bot_code/bot.py
@@ -3231,3 +3231,17 @@ async def leaderboard_command(ctx):
 
 
 bot.run(os.getenv('DISCORD_TOKEN'))
+
+from flask import Flask
+from threading import Thread
+
+app = Flask("")
+@app.route("/")
+def home
+    return "Bot is running"
+
+def run()
+    app.run(host="0.0.0.0",port=8080
+
+t = Thread(target=run)
+t.start()
